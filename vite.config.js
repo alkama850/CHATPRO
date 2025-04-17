0b88d7c16753b595cd674b09ffc12e8d7ee4b8a0
alkama844 <malkama844@gmail.com>
2025-04-17 23:21:01 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // এটির মাধ্যমে তুমি এন্ট্রি ফাইল সঠিকভাবে সেট করছো
+    },
+    outDir: 'dist',  // প্রোডাকশন বিল্ডের আউটপুট ডিরেক্টরি
+  },
 });
