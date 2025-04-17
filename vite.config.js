05d218e12851ab4196f9eae00406cddb70b6fc75
alkama844 <malkama844@gmail.com>
2025-04-17 23:10:59 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'), // This should be the relative path from the root directory
-    },
-    outDir: 'dist',
-  },
 });
